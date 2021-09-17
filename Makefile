a8f644655c458472e302b689bdc2434b49f75978
Hj3286913 <tan.tan@ucloud.cn>
2021-09-17 21:16:33 +0800

    begin

--- Makefile
+++ Makefile
@@ -133,6 +133,7 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_helloos\
+	$U/_trace\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/trace.c
@@ -0,0 +1,36 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(2, "Usage: trace mask cmd..\n");
+        exit(1);
+    }
+
+    // get mask (mask is syscall)
+    // fork
+    // set mask
+    // run cmd
+    int mask = atoi(argv[1]);
+
+    int pid;
+    pid = fork();
+    if (pid < 0)
+    {
+        printf("init: fork failed\n");
+        exit(1);
+    }
+
+    if (pid == 0)
+    {
+        exec(argv[2], &argv[2]);
+        printf("init: exec trace cmd failed\n");
+        exit(1);
+    }
+    fprintf(2, "trace pid %d mask %d\n", pid, mask);
+
+    exit(0);
+}
