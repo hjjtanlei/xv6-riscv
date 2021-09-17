a28ca8689d648cb843e903cbd46d9b59bf998ee9
Hj3286913 <tan.tan@ucloud.cn>
2021-09-17 22:07:33 +0800

    begin

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -170,7 +170,8 @@ syscall(void)
             p->pid, p->name, num);
     p->trapframe->a0 = -1;
   }
-
+ printf("%d:  mask->%d\n",
+            p->pid,p->syscall_mask);
   if ((num&(p->syscall_mask)) >0){
        printf("%d: syscall %s->%d\n",
             p->pid, syscalls_string[num] , p->trapframe->a0);
