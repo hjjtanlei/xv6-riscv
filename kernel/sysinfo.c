20c650ed6a761390fbe8947eed7b8f552d4912f7
Hj3286913 <tan.tan@ucloud.cn>
2021-09-19 23:53:13 +0800

    fix

--- kernel/sysinfo.c
+++ kernel/sysinfo.c
@@ -8,8 +8,8 @@
 int ksysinfo(uint64 addr)
 {
   printf("sysinfo addr %d \n", addr);
-  struct proc *p = myproc();
-  struct sysinfo st;
+  // struct proc *p = myproc();
+  //  struct sysinfo st;
 
   // if (copyout(p->pagetable, addr, (char *)&st, sizeof(st)) < 0)
   //   return -1;
