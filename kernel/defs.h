0bf8440383ee31742daa8818d710f7717267acc5
Hj3286913 <tan.tan@ucloud.cn>
2021-09-20 23:33:47 +0800

    fix

--- kernel/defs.h
+++ kernel/defs.h
@@ -108,7 +108,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             setmask(int pid,int mask);
-void            procinfo(sysinfo *info);
+void            procinfo(struct sysinfo *);
 // swtch.S
 void            swtch(struct context*, struct context*);
 
